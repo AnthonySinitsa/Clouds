2e8f39870ade7ee9e79cf08e1b84d27858a440bd
Anton <itonyemail@gmail.com>
2024-02-08 18:13:25 -0800

    perlin noise start

--- Assets/Scripts/PerlinNoiseGenerator.cs
+++ Assets/Scripts/PerlinNoiseGenerator.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PerlinNoiseGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PerlinNoiseGenerator : MonoBehaviour{
+
+    public int resolution = 10;
+    public float scale = 1f;
+    public float cubeSize = 1f;
+
+    void Start(){
+       GenerateTerrain(); 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void GenerateTerrain(){
+        for(int x = 0; x < resolution; x++){
+            for(int z = 0; z < resolution; z++){
+                float xCoord = transform.position.x + x * scale;
+                float zCoord = transform.position.z + z * scale;
+
+                float y = Mathf.PerlinNoise(xCoord, zCoord) * cubeSize;
+
+                Vector3 cubePosition = new Vector3(x * cubeSize, y, z * cubeSize);
+
+                InstantiateCube(cubePosition);
+            }
+        }
+    }
+
+    void InstantiateCube(Vector3 position){
+        GameObject cube = Instantiate(Resources.Load("CubePrefab") as GameObject);
+        cube.transform.position = position;
+        cube.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
     }
 }
