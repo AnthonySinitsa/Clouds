f974ed0c49b5115482e7b61f27cbb79d65469294
Anton <itonyemail@gmail.com>
2024-02-10 18:47:34 -0800

    changes to cloud script

--- Assets/Scripts/CloudNoiseGenerator.cs
+++ Assets/Scripts/CloudNoiseGenerator.cs
@@ -5,31 +5,32 @@ public class CloudNoiseGenerator : MonoBehaviour{
 
     public int resolution = 10;
     public float scale = 0.1f;
-    public float cubeSize = 0.1f;
+    public float cubeSizeMultiplier = 2f;
 
     void Start(){
-       GenerateTerrain(); 
+       GenerateCloud(); 
     }
 
-    void GenerateTerrain(){
-
-        float halfResolution = (resolution - 1) * cubeSize / 2f;
-
+    void GenerateCloud(){
         for(int x = 0; x < resolution; x++){
             for(int z = 0; z < resolution; z++){
                 float xCoord = transform.position.x + x * scale;
                 float zCoord = transform.position.z + z * scale;
 
-                float y = Mathf.PerlinNoise(xCoord, zCoord) * cubeSize * 10f;
+                float perlinValue = Mathf.PerlinNoise(xCoord, zCoord);
 
-                Vector3 cubePosition = new Vector3((x - halfResolution) * cubeSize, y, (z - halfResolution) * cubeSize);
+                float cubeSize = perlinValue * cubeSizeMultiplier;
 
-                InstantiateCube(cubePosition, transform);
+                cubeSize = Mathf.Max(cubeSize, 0.1f);
+
+                Vector3 cubePosition = new Vector3(x * cubeSize, 0f, z * cubeSize);
+
+                InstantiateCube(cubePosition, transform, cubeSize);
             }
         }
     }
 
-    void InstantiateCube(Vector3 position, Transform parent){
+    void InstantiateCube(Vector3 position, Transform parent, float cubeSize){
         GameObject cubePrefab = Resources.Load<GameObject>("CubePrefab");
 
         // Check if the prefab is found
