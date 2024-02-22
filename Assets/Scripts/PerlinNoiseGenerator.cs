ab22cafd6494f9e129729c644c4aeae34b966e6c
Anton <itonyemail@gmail.com>
2024-02-21 23:48:32 -0800

    time.time clouds change size

--- Assets/Scripts/PerlinNoiseGenerator.cs
+++ Assets/Scripts/PerlinNoiseGenerator.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-using System.Collections;
+
 public class PerlinNoiseGenerator : MonoBehaviour{
     public GameObject cubePrefab; // Drag your cube prefab here in the Inspector
     public int resolution; // Set the resolution here
     public float perlinScale; // Adjust the scale of the Perlin noise
     public float minCubeSize; // min size for cubes to spawn
+    public float scaleChangeSpeed = 0.5f; // Speed at which the cubes change size
 
     void Update(){
         ClearPlatform();
@@ -12,7 +13,7 @@ public class PerlinNoiseGenerator : MonoBehaviour{
     }
 
     void ClearPlatform(){
-        foreach(Transform child in transform){
+        foreach (Transform child in transform){
             Destroy(child.gameObject);
         }
     }
@@ -33,18 +34,22 @@ public class PerlinNoiseGenerator : MonoBehaviour{
                 float yPos = Mathf.PerlinNoise(xPos, zPos) * 2.0f;
 
                 // Use Perlin noise to determine the height variation
-                float scaleMultiplier = 
+                float scaleMultiplier =
                     Mathf.PerlinNoise(x * perlinScale, z * perlinScale) * 2.0f;
 
-                if(cubeSize * scaleMultiplier > minCubeSize){
+                if (cubeSize * scaleMultiplier > minCubeSize){
                     // Instantiate a cube prefab with adjusted scale
-                    GameObject cube = 
+                    GameObject cube =
                         Instantiate(cubePrefab, new Vector3(xPos, yPos, zPos), Quaternion.identity);
 
-                    // this var for better readibility for the transformation local var
+                    // this var for better readability for the transformation local var
                     float perlinCubeSize = cubeSize * scaleMultiplier;
-                    cube.transform.localScale = 
-                        new Vector3(perlinCubeSize, perlinCubeSize, perlinCubeSize);
+
+                    // Use a sine function to smoothly change the scale over time
+                    float dynamicScale = Mathf.Sin(Time.time * scaleChangeSpeed) * 0.25f + 1.0f;
+
+                    cube.transform.localScale =
+                        new Vector3(perlinCubeSize * dynamicScale, perlinCubeSize * dynamicScale, perlinCubeSize * dynamicScale);
 
                     // Make the cube a child of the prefab cube
                     cube.transform.parent = transform;
